965d7a01b18de2950791a943c686998a62b3222b
Marco Masik <marcomasik@gmail.com>
2025-11-25 15:22:22 +0700

    Add style file to start page, which is page.tsx in src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import "./start-page.scss";
+
 export default function StartPage() {
   return (
     <div className="start-page-container">
       <div className="video-box-container">
-        <video src="videos/ocean_waves.mp4" autoPlay playsInline muted loop />
+        <video src="/videos/ocean_waves.mp4" autoPlay playsInline muted loop />
       </div>
     </div>
   );
