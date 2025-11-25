97c9ab30d30c5608369c12f29df877f64e1412dd
Marco Masik <marcomasik@gmail.com>
2025-11-25 16:39:25 +0700

    Use css clamp to fit title to window size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function StartPage() {
       <div className="video-box-container">
         <video id="ocean-waves-video" src="/videos/ocean_waves.mp4" autoPlay playsInline muted loop />
         <div className="text-container">
-          <h1 className="title">MARCO MASIK</h1>
+          <h1 className="title">MARCO&nbsp;MASIK</h1>
           <h2 className="subtitle">FULL STACK DEVELOPER</h2>
           <ul className="start-page-menu">
             <li><a href="#works">WORKS</a></li>
