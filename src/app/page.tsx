33a580f8f0dfe8b7fff72e5cc2d9ac16f4c83cf5
Marco Masik <marcomasik@gmail.com>
2025-11-25 16:07:23 +0700

    Add styles from old portfolio to texts on the start page video box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ export default function StartPage() {
     <div className="start-page-container">
       <div className="video-box-container">
         <video id="ocean-waves-video" src="/videos/ocean_waves.mp4" autoPlay playsInline muted loop />
+        <div className="text-container">
+          <h1 className="title">MARCO MASIK</h1>
+          <h2 className="subtitle">FULL STACK DEVELOPER</h2>
+          <ul className="start-page-menu">
+            <li><a href="#works">WORKS</a></li>
+            <li><a href="#cv">CV</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
