ded1a9d07cdc779e527e563d2e10b755df0ce368
Marco Masik <marcomasik@gmail.com>
2025-11-26 17:27:24 +0700

    Use fittext to size subtitle and menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,20 @@ export default function StartPage() {
       <div className="video-box-container">
         <video id="ocean-waves-video" src="/videos/ocean_waves.mp4" autoPlay playsInline muted loop />
         <div className="text-container">
-        <Textfit mode="single" min={20} max={120}>
-          <h1 className="title">MARCO&nbsp;MASIK</h1>
+          <Textfit mode="single" min={16} max={120}>
+            <h1 className="title">MARCO&nbsp;MASIK</h1>
           </Textfit>
-
-          <h2 className="subtitle">FULL STACK DEVELOPER</h2>
+          <div className="subtitle-container">
+            <Textfit mode="single" min={8} max={120}>
+              <h2 className="subtitle">FULL STACK DEVELOPER</h2>
+            </Textfit>
+          </div>
           <ul className="start-page-menu">
-            <li><a href="#works">WORKS</a></li>
-            <li><a href="#cv">CV</a></li>
-            <li><a href="#contact">CONTACT</a></li>
+            <Textfit mode="single" min={8} max={120}>
+              <li><a href="#works">WORKS</a></li>
+              <li><a href="#cv">CV</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+            </Textfit>
           </ul>
         </div>
       </div>
