5406848e93f0bc637b6f6b2dfaa0216eb9720b64
Marco Masik <marcomasik@gmail.com>
2025-11-29 21:13:44 +0700

    Add structure to works section

--- src/components/sections/WorkSection/WorksSection.tsx
+++ src/components/sections/WorkSection/WorksSection.tsx
@@ -4,7 +4,13 @@ export const WorksSection = () => {
   return (
     <section id="works" className="section">
         <div className="works-container">
-
+          <div className="works-container-inner">
+            <div className="works-container-content">
+              <h1>WORKS</h1>
+              <h2>Full time jobs</h2>
+              <h2>Freelance website projects</h2>
+            </div>
+          </div>
         </div>
     </section>
   );
