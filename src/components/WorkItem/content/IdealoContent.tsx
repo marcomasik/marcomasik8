efca925a25742ee05b3444fbe1b69d77b175c9a7
Marco Masik <marcomasik@gmail.com>
2025-12-04 13:06:50 +0700

    Add image and fix layout

--- src/components/WorkItem/content/IdealoContent.tsx
+++ src/components/WorkItem/content/IdealoContent.tsx
@@ -12,6 +12,26 @@ export const IdealoContent = () => {
           <li>AB Testing</li>
           <li>UX</li>
         </ul>
+        <div className="work-item-expanded-case-study-container">
+          <div className="work-item-case-study-blob">
+            <h5>Context 🧩</h5>
+            <div className="case-study-blob-row">
+              <p>
+                Idealo aimed to expand its price comparison platform by adding Local Offers: showing prices from nearby shops without reducing online conversions.
+              </p>
+              <div className="case-study-blob-image-container">
+                <img src="/images/axel_springen_neubau.jpeg" alt="Idealo Building" />
+              </div>
+            </div>
+            <ul>
+              <li>Legacy stack: JSP, jQuery, Java Spring Boot</li>
+              <li>Privacy-friendly location detection</li>
+              <li>Onboarding local vendors</li>
+            </ul>
+        
+          </div>
+
+        </div>
       </div>
     );
   };
