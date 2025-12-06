6d19ffeb84277eba0dc0f7ad7458e3818c6ccdc1
Marco Masik <marcomasik@gmail.com>
2025-12-06 12:36:56 +0700

    Add miles title to work item

--- src/components/WorkItem/content/MilesContent.tsx
+++ src/components/WorkItem/content/MilesContent.tsx
@@ -3,11 +3,7 @@ import "./work-item-content.scss";
 export const MilesContent = () => {
     return (
             <div className="work-item-expanded-content">
-                <h2>MilesContent</h2>
-                <h3>MilesContent</h3>
-                <h4>MilesContent</h4>
-                <h5>MilesContent</h5>
-                <h6>MilesContent</h6>
+                <h4>Web Developer // Miles Mobility GmbH, Berlin</h4>
             </div>
     );
 };
