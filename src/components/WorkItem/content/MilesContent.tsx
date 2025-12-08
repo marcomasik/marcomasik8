e743b45d9b958f0469690478b11ec84aeda13f5e
Marco Masik <marcomasik@gmail.com>
2025-12-08 19:50:36 +0700

    Fix structure in miles content

--- src/components/WorkItem/content/MilesContent.tsx
+++ src/components/WorkItem/content/MilesContent.tsx
@@ -4,13 +4,19 @@ export const MilesContent = () => {
     return (
             <div className="work-item-expanded-content">
                 <h4>Web Developer // Miles Mobility GmbH, Berlin</h4>
-                <div className="work-item-case-study-blob">
-                    <h5>Context 🚗</h5>
-                    <p>
-                    Miles Mobility is one of Germany’s largest independent car-sharing companies. Notable for being one of the few not backed by a car manufacturer. Operating across many German cities, its unique pay per distance model sets it apart from competitors.
-
-As the company scaled, the marketing team needed a new website that could handle dynamic campaigns, achieve top SEO scores, and reflect the bold, youthful identity of the brand.                    </p>
+                <div className="work-item-expanded-case-study-container">
+                    <div className="work-item-case-study-blob">
+                        <h5>Context 🚗</h5>
+                        <p>
+                            Miles Mobility is one of Germany’s largest independent car-sharing companies. 
+                            Notable for being one of the few not backed by a car manufacturer. Operating across many German cities, 
+                            its unique pay per distance model sets it apart from competitors.
+                            As the company scaled, the marketing team needed a new website that could handle dynamic campaigns, achieve top SEO scores, 
+                            and reflect the bold, youthful identity of the brand.                    
+                        </p>
+                    </div>  
                 </div>
             </div>
+
     );
-};
+};  
