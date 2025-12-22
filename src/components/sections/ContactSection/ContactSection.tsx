34dc671f2eadab4a4a94df0d6f10b5f098976429
Marco Masik <marcomasik@gmail.com>
2025-12-22 22:47:36 +0700

    Style the contact

--- src/components/sections/ContactSection/ContactSection.tsx
+++ src/components/sections/ContactSection/ContactSection.tsx
@@ -1,19 +1,33 @@
+import "./contact-section.scss";
+
 export const ContactSection = () => {
   return (
     <section id="contact" className="section">
       <div className="contact-container">
         <div className="contact-container-inner">
         <h2>Contact</h2>
-          <div className="contact-container-inner-left">
-            <p>Get in touch with me here:</p>
-            <div className="contact-container-inner-left-links">
-              <a href="https://www.linkedin.com/in/marco-masik-8787878787/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-              <a href="https://github.com/marco-masik" target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href="https://www.instagram.com/marco.masik/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <div className="contact-container-content">
+            <div className="contact-container-inner-left">
+              <p>Get in touch with me here:</p>
+              <p>hello@marcomasik.com</p>
+              <div className="contact-container-inner-left-links">
+                <a href="http://linkedin.com/in/marco-masik/" target="_blank" rel="noopener noreferrer">
+                  <div className="contact-container-inner-left-links-icon">
+                    <img src="/images/logos/linkedin_logo.png" alt="LinkedIn" />
+                  </div>
+                  LinkedIn
+                </a>
+                <a href="https://github.com/marco-masik" target="_blank" rel="noopener noreferrer">
+                  <div className="contact-container-inner-left-links-icon">
+                    <img src="/images/logos/gitHub_logo.png" alt="GitHub" />
+                  </div>
+                  GitHub
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="contact-container-inner-right">
-            <img src="/images/contact-image.png" alt="Contact" />
+            <div className="contact-container-inner-right">
+              <img src="/images/contact-image.png" alt="Contact" />
+            </div>
           </div>
         </div>
       </div>
