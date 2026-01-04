af62fbdeec9d4dd6a8a01ba2527a33ee322dc2c2
Marco Masik <marcomasik@gmail.com>
2026-01-04 16:19:12 +0700

    Let have title a line break on mobile on the start page

--- src/components/sections/StartSection/StartSection.tsx
+++ src/components/sections/StartSection/StartSection.tsx
@@ -15,9 +15,12 @@ export const StartSection = () => {
         <div className="video-box-container">
           <video id="ocean-waves-video" src="/videos/ocean_waves.mp4" autoPlay playsInline muted loop />
           <div className="text-container">
-            <Textfit mode="single" min={16} max={120}>
+            <Textfit mode="single" min={16} max={120} className="title-desktop">
               <h1 className="title">MARCO&nbsp;MASIK</h1>
             </Textfit>
+            <h1 className="title title-mobile">
+              MARCO<br />MASIK
+            </h1>
             <ul className="start-page-menu">
               <Textfit mode="single" min={8} max={120}>
                 <li><a href="#works" onClick={(e) => handleClick(e, 'works')}>WORKS</a></li>
